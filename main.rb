39a4a8972285d7e919c700ed897316b839ced54d
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-05-08 21:36:21 -0700

    added button class and initializer

--- main.rb
+++ main.rb
@@ -7,4 +7,16 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
       paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
     end
   end
-  
+class Button
+    def initialize(label_text, x, y, foreground_color)
+        @label_text = label_text
+        @x = x
+        @y = y
+        @foreground_color = foreground_color
+    end
+
+    
+
+
+    
+end  
