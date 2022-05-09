c31f2184ac1d03e80b131a3e871564f6bb2083ba
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-05-08 21:41:32 -0700

    non-dark mmode function added to the button class

--- main.rb
+++ main.rb
@@ -18,7 +18,9 @@ class Button
     def dark_mode(label_text, x, y, foreground_color)
         paint(label_text, x, y, foreground_color - 10, '#111111')
     end
-  
+    def non_dark_mode(label_text, x, y, foreground_color)
+        paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    end
 
 
     
